155ad114f8edc7c615015e8e59c8c1f5f7a40de6
waqarfaooq444 <waqarfarooq444@icloud.com>
2023-06-19 23:30:54 +0500

    Assignment#1_Completed_Alhamdulillah

--- Sir Bilal/main.dart
+++ Sir Bilal/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math'; // helped since question 11.
+
 void main() {
   int length = 12; //Q1
   int breadth = 13; //Q1
@@ -14,6 +16,36 @@ void main() {
   var temperature = 11; //Q5
 
   var alphabet = "b"; //Q6
+
+  String Cid = "1001"; //Q7
+  String ConsumerName = "James"; //Q7
+  int UnitsConsumed = 800; //Q7
+  double Payable = 0; //Q7
+
+// Q8
+  String SN = "Waqar";
+  String RN = "24552";
+  String cls = "16";
+  var grd = "";
+  num English = 77;
+  num Maths = 82;
+  num Science = 79;
+  num Economics = 90;
+  num Accounts = 88;
+
+// Q9
+  int nO = 35;
+
+// Q10
+  int Num1 = 400;
+  int Num2 = 200;
+  int Num3 = 300;
+
+// Q11 done with help of https://www.youtube.com/watch?v=ptdlRZVkRls
+  int number = 10;
+
+// Q12
+  int tempInCelsius = 35;
 
 //  Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
 // ie: if both values are equal then it's square otherwise rectangle.
@@ -106,18 +138,99 @@ void main() {
 // Amount Charges @Rs. 2.00 per unit : 1600.00
 // Net Bill Amount : 1600.00
 
+  if (UnitsConsumed <= 199) {
+    Payable = UnitsConsumed * 1.20;
+  } else if (UnitsConsumed >= 200 && UnitsConsumed < 400) {
+    Payable = UnitsConsumed * 1.50;
+  } else if (UnitsConsumed >= 400 && UnitsConsumed < 600) {
+    Payable = UnitsConsumed * 1.80;
+  } else if (UnitsConsumed >= 600) {
+    Payable = UnitsConsumed * 2.00;
+  }
+  print("Ans#7");
+  print("Consumer ID: $Cid");
+  print("Consumer Name: $ConsumerName");
+  print("Net Bill: $Payable");
+
 // Q8: Create a marksheet using operators of at least 5 subjects and output should have Student Name, Student Roll Number, Class, Percentage, Grade Obtained etc.
 // i.e: Percentage should be rounded upto 2 decimal places only.
 
+  num TmObtained = English + Maths + Science + Economics + Accounts;
+  double percent = TmObtained / 500 * 100;
+
+  print("Ans#8");
+  print("Student Name: $SN");
+  print("Student Roll Number: $RN");
+  print("Class: $cls");
+  print("Total Marks Obtained: $TmObtained");
+  print("Percentage: $percent%");
+
+  if (percent >= 85 && percent <= 100) {
+    print("Grade: $grd A");
+  } else if (percent > 75 && percent <= 84) {
+    print("Grade: $grd B");
+  } else if (percent >= 65 && percent <= 74) {
+    print("Grade: $grd C");
+  } else if (percent >= 50 && percent <= 64) {
+    print("Grade: $grd D");
+  } else if (percent > 0 && percent <= 49) {
+    print("Grade: $grd F");
+  }
 // Q9: Check if the number is even or odd, If number is even then check if this is divisible by 5 or not & if number is odd then check if this is divisible by 7 or not.
 
+  print("Ans#9");
+  print("Provided Number: $nO");
+  if (nO.isEven) {
+    print("Number is even");
+  } else
+    print("Number is odd");
+
+  if (nO % 5 == 0 && nO % 7 == 0) {
+    print("$nO is divisible by both, 5 and 7");
+  }
+  if (nO % 5 == 0) {
+    print("$nO is divisible by 5");
+  }
+  if (nO % 7 == 0) {
+    print("$nO is divisible by 7");
+  }
+
 // Q10: Write a program that takes three numbers from the user and prints the greatest number & lowest number.
+
+  print("Ans#10");
+  if (Num1 > Num2 && Num1 > Num3) {
+    print("$Num1 is Greatest number");
+  }
+  if (Num2 > Num1 && Num2 > Num3) {
+    print("$Num2 is Greatest number");
+  }
+  if (Num3 > Num2 && Num3 > Num1) {
+    print("$Num3 is Greatest number");
+  }
+
+  if (Num1 < Num2 && Num1 < Num3) {
+    print("$Num3 is the Lowest number");
+  }
+  if (Num2 < Num1 && Num2 < Num3) {
+    print("$Num3 is the Lowest number");
+  }
+  if (Num3 < Num2 && Num3 < Num1) {
+    print("$Num3 is the Lowest number");
+  }
 
 // Q11: Write a program to calculate root of any number.
 // i.e: √y = y½
 
+  num squareRoot = sqrt(number);
+  print("Ans#11");
+  print(squareRoot);
+
 // Q12: Write a program to convert Celsius  to Fahrenheit   .
 // i.e: Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+
+  num cnversion = tempInCelsius * 9 / 5 + 30;
+  print("Ans#12");
+  print("$cnversion° Fahrenheit ");
 
 // Q13: Set and Up in github
 }
